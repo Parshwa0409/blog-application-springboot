444cb87a77e5f8f30a3f23cf64dad9dd19ed01ca
Parshwa0409 <parshwapatil9@gmail.com>
2025-12-25 21:37:26 +0530

    user implementes userDetails

--- src/main/java/patil/parshwa/blog/models/User.java
+++ src/main/java/patil/parshwa/blog/models/User.java
@@ -8,16 +8,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "users")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String email;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 }

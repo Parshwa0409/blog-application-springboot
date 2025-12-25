ad4c00421e131bb9d78400b6641980fcdefa4b05
Parshwa0409 <parshwapatil9@gmail.com>
2025-12-25 21:38:23 +0530

    adding findByUsername method

--- src/main/java/patil/parshwa/blog/repositories/UserRepository.java
+++ src/main/java/patil/parshwa/blog/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package patil.parshwa.blog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import patil.parshwa.blog.models.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }

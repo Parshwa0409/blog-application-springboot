b9e236870daff13f5ccdbf45d2501c91e097f0c6
Parshwa0409 <parshwapatil9@gmail.com>
2026-01-02 15:00:48 +0530

    creating admin user - alice

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,6 @@ INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
 
 INSERT into users (username, email, password) VALUES ('Alice', 'alice@gamil.com', '$2a$12$9CHk.045yPPmn82kA6L5H.o5JqZkhySM7genfaE6WcD86mYIpB/pm');
+
+INSERT INTO user_roles(user_id, role_id) VALUES (1, 1);
+INSERT INTO user_roles(user_id, role_id) VALUES (1, 2);
